9fd8feb8165b04b430e477d3bbc0e10c7a7770be
Filipe Relvas <frelvas5@gmail.com>
2014-03-23 16:20:08 +0000

    Renamed and re-organized some functions Minor fixes

--- src/ist/meic/pa/Inspector.java
+++ src/ist/meic/pa/Inspector.java
@@ -1,6 +1,7 @@
 package ist.meic.pa;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -14,6 +15,7 @@ public class Inspector {
 	private Object objectInstance;
 	private ArrayList<Object> objectHistory;
 	private HashMap<String,Field> objectFields;
+	private HashMap<String,Method> objectMethods;
 	
 	public Inspector(){}
 
@@ -27,18 +29,10 @@ public class Inspector {
 
 		while(true) {
 
-			//Print class name
-			if(objectInstance.toString().equals("")) {
-				System.err.println("Instance of " + objectInstance.getClass());
-			} else {
-				System.err.println(objectInstance + " is an instance of " + objectInstance.getClass());
-			}
-			
-			//Print class fields	
-			this.printFields(objectFields);
+			this.printObjectFeatures(objectInstance, objectFields);
 
-			/* User interaction */
-			System.out.print("> ");
+			/*** User interaction ***/
+			System.err.print("\n> ");
 
 			userInput = scanner.next();
 
@@ -49,7 +43,7 @@ public class Inspector {
 
 				userInput = scanner.next();
 
-				objectHistory.add(objectInstance.getClass());
+				objectHistory.add(objectInstance);
 				
 				objectInstance = this.createInstance(objectFields.get(userInput).getType());
 				objectFields = this.getFields(objectInstance.getClass());
@@ -60,26 +54,6 @@ public class Inspector {
 				
 			}
 		}
-	}
-
-	/**
-	 * Get all the class fields and make them accessible
-	 * 
-	 * @param clazz The class object
-	 * @return All class fields
-	 */
-	private HashMap<String,Field> getFields(Class<?> clazz) {
-
-		HashMap<String,Field> fields = new HashMap<String,Field>();
-
-		for(Class<?> c = clazz; c != null; c = c.getSuperclass()) {
-			for(Field f : c.getDeclaredFields()){
-				if(!f.isAccessible())
-					f.setAccessible(true);
-				fields.put(f.getName(),f);
-			}
-		}
-		return fields;
 	}
 
 	/**
@@ -103,12 +77,43 @@ public class Inspector {
 	}
 	
 	/**
-	 * Print the fields contained in the hashmap received as argument
+	 * Get all the class fields and make them accessible
 	 * 
+	 * @param clazz The class object
+	 * @return All class fields
+	 */
+	private HashMap<String,Field> getFields(Class<?> clazz) {
+
+		HashMap<String,Field> fields = new HashMap<String,Field>();
+
+		for(Class<?> c = clazz; c != null; c = c.getSuperclass()) {
+			for(Field f : c.getDeclaredFields()){
+				if(!f.isAccessible())
+					f.setAccessible(true);
+				fields.put(f.getName(),f);
+			}
+		}
+		return fields;
+	}
+
+	/**
+	 * Prints object features, name, fields, methods
+	 * 
+	 * @param objectInstance The object instance
 	 * @param hashMap The field hashmap
 	 */
-	private void printFields(HashMap<String,Field> hashMap) {
+	private void printObjectFeatures(Object objectInstance, HashMap<String,Field> hashMap) {
+		
+		//Print object name and class instance
+		if(objectInstance.toString().equals("")) {
+			System.err.println("Instance of " + objectInstance.getClass());
+		} else {
+			System.err.println(objectInstance + " is an instance of " + objectInstance.getClass());
+		}	
 			
+		System.err.println("-------------");
+		
+		//Print object fields
 		for(Field field : hashMap.values()) {	
 			try {
 				String fieldsOutput = field.getType() + " " + field.getName() + " = " + field.get(objectInstance);
@@ -124,5 +129,16 @@ public class Inspector {
 				System.err.println(e.getMessage());
 			}
 		}	
+	}
+	
+	/**
+	 * Modifies the value of the field named fieldName so that it becomes value
+	 * 
+	 * @param objectInstance The object instance
+	 * @param fieldName The object field name to be re-valued
+	 * @param value The new object field value
+	 */
+	private void fieldTypeModifier(Object objectInstance, String fieldName, String value) {
+		
 	}
 }
